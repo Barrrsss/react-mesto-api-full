beb4b31d3022930dfc0038f21403a9bb79ef5d73
Aleksey Krasnov <neky@list.ru>
2021-04-17 16:38:32 +0500

    fix

--- backend/app.js
+++ backend/app.js
@@ -21,14 +21,6 @@ app.use(express.json(), cors(corsOptions));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS',
-  );
   next();
 });
 
